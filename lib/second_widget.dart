f450c0473228d8770430e953e156967916cffb94
ben batundi <benbatundi7@gmail.com>
2022-03-17 15:45:52 +0100

    second change

--- lib/second_widget.dart
+++ lib/second_widget.dart
@@ -7,6 +7,7 @@ class SecondWidget extends StatefulWidget {
 }
 
 class _SecondWidgetState extends State<SecondWidget> {
+  int incremente = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +17,22 @@ class _SecondWidgetState extends State<SecondWidget> {
       ),
       appBar: AppBar(
         title: const Text("C-store"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Nombre incrementé"),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "$incremente",
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
